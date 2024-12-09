e8dee41e3412de8386c546fdbf2481c18bf5e5b4
Prinve59 <149373739+Prinve59@users.noreply.github.com>
2024-12-10 00:42:00 +0530

    Update models.py

--- attendance/models.py
+++ attendance/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    lib_id=models.CharField(max_length=15)
+    name=models.CharField(max_length=20)
+    frequency=models.CharField(max_length=20,blank=1,default=1)
+
+    def __str__(self):
+        return self.lib_id
