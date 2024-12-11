f4a06125bb1d9cfd0399a889d0e0a13f56edf76a
Prinve59 <149373739+Prinve59@users.noreply.github.com>
2024-12-11 22:35:00 +0530

    50_models.py

--- attendance/models.py
+++ attendance/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Contact(models.Model):
-    lib_id=models.CharField(max_length=25)
+    lib_id=models.CharField(max_length=50)
     name=models.CharField(max_length=50,default=1)
     frequency=models.CharField(max_length=20,blank=1)
 
