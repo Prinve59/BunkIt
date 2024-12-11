3c23c7773ee40a15ac982147d93ff28559c2d130
Prinve59 <149373739+Prinve59@users.noreply.github.com>
2024-12-11 22:41:24 +0530

    2models.py

--- attendance/models.py
+++ attendance/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Contact(models.Model):
     lib_id=models.CharField(max_length=50)
     name=models.CharField(max_length=50,default=1)
-    frequency=models.CharField(max_length=20,blank=1)
+    frequency=models.CharField(max_length=20,default=1,blank=1)
 
     def __str__(self):
         return self.lib_id
