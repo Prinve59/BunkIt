6305bce2b14925b23edc19931178fe6c844d8e07
Prinve59 <149373739+Prinve59@users.noreply.github.com>
2025-05-21 15:13:53 +0530

    Update forms.py

--- attendance/forms.py
+++ attendance/forms.py
@@ -3,3 +3,8 @@ from django import forms
 class LoginForm(forms.Form):
     username = forms.CharField(label="Library ID", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
     password = forms.CharField(label="Password", max_length=100, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    without_event = forms.BooleanField(
+        label="Without Event and Medical",
+        required=False,  # Optional field
+        widget=forms.CheckboxInput(attrs={'class': 'form-check-input ms-2 mt-2'}),
+    )
